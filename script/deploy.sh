343e1daa01fff7eed996ae85fa9d200eb3eb6f71
Han Cho <chohann@gmail.com>
2020-10-10 16:23:27 -0700

    Task #10 - 3
    Task #10 - 3

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env #!/usr/bin/env bash
 set -e
 
-echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin
+docker login --username ${DOCKER_LOGIN} --password "${DOCKER_PASSWORD}"
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/web1066-the-app:latest
 docker push $DOCKER_LOGIN/web1066-the-app:latest
 set +e
